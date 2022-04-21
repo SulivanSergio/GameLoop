cb411b1bccd7437beabbdb90bd50c8eb16e3c053
Sulivan Sergio <sulivansergio22@gmail.com>
2022-04-21 09:45:55 -0300

    Criação do gameLoop no script GameLoop

--- src/W5/GameLoop.java
+++ src/W5/GameLoop.java
@@ -1,12 +1,61 @@
 package W5;
 
 public class GameLoop {
+	
+	
+	float position = 0;
+	float speed = 10.0f;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("opa");
+		GameLoop gl = new GameLoop();
+		gl.Start();
 		
 	}
+	
+	
+	public void Input()
+	{
+		
+	}
+	
+	public void Update(float deltaTime)
+	{
+		
+		position = position + speed * deltaTime;
+		
+	}
+	
+	public void Render()
+	{
+		
+		
+		
+	}
+	
+	
+	public void Start()
+	{
+		float deltaTime = 0;
+		
+		while(true)
+		{
+			long start = System.currentTimeMillis();
+			
+			Input();
+			Update(deltaTime);
+			Render();
+			
+			long end = System.currentTimeMillis();
+			deltaTime = (float) (end - start)/ 1000.0f;
+			
+		}
+		
+		
+	}
+	
+	
 
 }
