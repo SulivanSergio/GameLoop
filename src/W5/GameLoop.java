c8774334456227b6e54a89e24b13c8d6ad8ee8d8
Sulivan Sergio <sulivansergio22@gmail.com>
2022-04-21 10:52:18 -0300

    Criação do FixedUpdate

--- src/W5/GameLoop.java
+++ src/W5/GameLoop.java
@@ -5,7 +5,10 @@ public class GameLoop {
 	
 	float position = 0;
 	float speed = 10.0f;
+	final int SYNC = 30;
 	
+	int sync1 = 0;
+	float soma = 0.0f;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,14 +27,19 @@ public class GameLoop {
 	public void Update(float deltaTime)
 	{
 		
-		position = position + speed * deltaTime;
 		
+		
+	}
+	public void FixedUpdate(float fixedDeltaTime)
+	{
+		position = position + speed * fixedDeltaTime;
+		System.out.println("Fisica do jogo: " + position);
 	}
 	
 	public void Render()
 	{
 		
-		
+		//System.out.println("Fisica do jogo: " + position);
 		
 	}
 	
@@ -40,16 +48,36 @@ public class GameLoop {
 	{
 		float deltaTime = 0;
 		
+		
 		while(true)
 		{
 			long start = System.currentTimeMillis();
 			
+			
+			
 			Input();
 			Update(deltaTime);
+			if(soma <= 1.0f)
+			{
+				if(sync1 < SYNC)
+				{
+					sync1 += 1;
+					FixedUpdate(deltaTime);
+					
+				}
+				
+			}else
+				{
+					soma = 0.0f;
+					sync1 = 0;
+				}
 			Render();
 			
 			long end = System.currentTimeMillis();
 			deltaTime = (float) (end - start)/ 1000.0f;
+			soma += 0.0001f ;
+			//System.out.println(sync1);
+			
 			
 		}
 		
